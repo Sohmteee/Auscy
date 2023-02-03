18be8580a1d57cf164b5dd77d29276f7220530c4
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-03 02:54:12 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -123,9 +123,7 @@ class _ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    setState(() {
-      buildReplyHoverBubble();
-    });
+    
     return Scaffold(
         appBar: AppBar(title: const Text("ChatGPT")),
         body: SafeArea(
