a4cdfbebc4013f96553256733f2c279e74cf4008
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-04 20:25:41 +0100

    commit

--- lib/provider.dart
+++ lib/provider.dart
@@ -2,4 +2,16 @@ import 'package:chat_gpt_02/chatmessage.dart';
 import 'package:chat_gpt_02/reply_hover_bubble.dart';
 import 'package:flutter/material.dart';
 
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
 
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
