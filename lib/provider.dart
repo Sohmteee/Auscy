b5757d3c9bb7cc0c80869521673e6b147d490c88
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-04 20:26:11 +0100

    commit

--- lib/provider.dart
+++ lib/provider.dart
@@ -3,7 +3,23 @@ import 'package:chat_gpt_02/reply_hover_bubble.dart';
 import 'package:flutter/material.dart';
 
 class MyProvider extends StatefulWidget with ChangeNotifier {
-   MyProvider({super.key});
+  MyProvider({
+    super.key,
+    String sender;
+  String text;
+  void Function(bool) setResponse;
+  void Function(ChatMessage) setReplyMessage;
+
+  MyProvider({
+    required this.sender,
+    required this.text,
+    required this.setResponse,
+    required this.setReplyMessage,
+  });
+
+  ReplyHoverBubble replyHoverBubble = ReplyHoverBubble(sender: sender, text: text, setResponse: setResponse, setReplyMessage: setReplyMessage);
+
+  });
 
   @override
   State<MyProvider> createState() => _MyProviderState();
