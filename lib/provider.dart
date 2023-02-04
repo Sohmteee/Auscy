435b08a36cb4c1b3b2ec039535928b9b0414c0fb
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-04 20:25:32 +0100

    commit

--- lib/provider.dart
+++ lib/provider.dart
@@ -2,19 +2,4 @@ import 'package:chat_gpt_02/chatmessage.dart';
 import 'package:chat_gpt_02/reply_hover_bubble.dart';
 import 'package:flutter/material.dart';
 
-class MyProvider extends ChangeNotifier {
-  String sender;
-  String text;
-  void Function(bool) setResponse;
-  void Function(ChatMessage) setReplyMessage;
 
-  MyProvider({
-    required this.sender,
-    required this.text,
-    required this.setResponse,
-    required this.setReplyMessage,
-  });
-
-  ReplyHoverBubble replyHoverBubble = ReplyHoverBubble(sender: sender, text: text, setResponse: setResponse, setReplyMessage: setReplyMessage);
-
-}
