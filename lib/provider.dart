9dae94f81850c24eb18482a10ac268a7dfd5ea6d
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-04 20:27:41 +0100

    commit

--- lib/provider.dart
+++ lib/provider.dart
@@ -2,7 +2,7 @@ import 'package:chat_gpt_02/chatmessage.dart';
 import 'package:chat_gpt_02/reply_hover_bubble.dart';
 import 'package:flutter/material.dart';
 
-class MyProvider extends StatelessWidget with ChangeNotifier {
+class MyProvider extends  ChangeNotifier {
   MyProvider({
     super.key,
     required this.sender,
