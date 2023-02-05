76ca108247ce030edebaa7b4052700b41296d0ab
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-05 23:18:20 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -63,7 +63,7 @@ class _ChatScreenState extends State<ChatScreen> {
     String prmpt = "";
 
     for (int index = 0; index < 20; index++) {
-      ChatMessage msg = _messages[index];
+      ChatMessage msg = _messages[index].text.trim();
       promptList.add(msg);
     }
 
