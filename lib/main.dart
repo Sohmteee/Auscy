313b84ce7d040d093a1437f23048db35e512f70e
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-06 09:04:52 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => 
+      create: (context) => Chat
       child: MaterialApp(
         title: 'ChatGPT',
         debugShowCheckedModeBanner: false,
