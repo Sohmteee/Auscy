9d9fccef26c00295ee32d2c56e0c63e5e495c33c
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-07 09:08:38 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -153,7 +153,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
                   children: [
-                    Consumer<ChatMessageData>(child: buildReplyHoverBubble()),
+                    Consumer<ChatMessageData>(builder: (context, data, child)(child: buildReplyHoverBubble()),
                     Container(
                       decoration: BoxDecoration(
                         color: Vx.zinc200,
