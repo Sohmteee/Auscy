70494211f070ee783cfef5cf1948c17da17ef764
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-07 09:25:13 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -59,17 +59,12 @@ class _ChatScreenState extends State<ChatScreen> {
 
     _controller.clear();
 
-    String prmpt = "";
+    /* String prmpt = "";
 
     List<String> promptList =
         _messages.take(20).map((msg) => msg.text.trim()).toList();
 
-
-
-
-    String prmpt = _messages.length > 20
-        ? _messages.take(20).join('\n')
-        : _messages.join('\n'); 
+    prmpt = promptList.join('\n'); */
 
     final request =
         CompleteText(prompt: message.text, model: "text-davinci-003");
