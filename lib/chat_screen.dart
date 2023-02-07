55babbcd01f0cc77ae8cdfe5711c23e820426bdf
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-07 09:24:45 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -122,7 +122,7 @@ class _ChatScreenState extends State<ChatScreen> {
     });
   }
 
-  Widget buildReplyPreview() {
+  Widget buildReplyHoverBubble() {
     return Consumer<ChatMessageData>(builder: (context, data, child) {
       return data.isReply(chatMessage)
           ? ReplyPreview(
@@ -156,7 +156,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
                   children: [
-                    buildReplyPreview(),
+                    buildReplyHoverBubble(),
                     Container(
                       decoration: BoxDecoration(
                         color: Vx.zinc200,
