38d3ffb379a219347afc3d1501ec3f356b45b104
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-10 17:41:33 +0100

    commit

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -33,31 +33,35 @@ class _ChatMessageState extends State<ChatMessage> {
           debugPrint("Replying to: ${replyMessage!.text}");
         });
       },
-      child: ChatBubble(
-        clipper: ChatBubbleClipper8(
-            type: widget.sender == MessageSender.user
-                ? BubbleType.sendBubble
-                : BubbleType.receiverBubble),
-        alignment: widget.sender == MessageSender.user
-            ? Alignment.topRight
-            : Alignment.topLeft,
-        margin: const EdgeInsets.only(top: 20),
-        backGroundColor:
-            widget.sender == MessageSender.user ? Vx.green500 : Vx.zinc200,
-        child: Container(
-          constraints: BoxConstraints(
-            maxWidth: MediaQuery.of(context).size.width * 0.7,
-          ),
-          child: Text(
-            widget.text.trim(),
-            style: TextStyle(
-              color: widget.sender == MessageSender.user
-                  ? Colors.white
-                  : Colors.black,
-              fontSize: 16,
+      child: Row(
+        children: [
+          ChatBubble(
+            clipper: ChatBubbleClipper8(
+                type: widget.sender == MessageSender.user
+                    ? BubbleType.sendBubble
+                    : BubbleType.receiverBubble),
+            alignment: widget.sender == MessageSender.user
+                ? Alignment.topRight
+                : Alignment.topLeft,
+            margin: const EdgeInsets.only(top: 20),
+            backGroundColor:
+                widget.sender == MessageSender.user ? Vx.green500 : Vx.zinc200,
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.7,
+              ),
+              child: Text(
+                widget.text.trim(),
+                style: TextStyle(
+                  color: widget.sender == MessageSender.user
+                      ? Colors.white
+                      : Colors.black,
+                  fontSize: 16,
+                ),
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
