fc032d5f1d41bb4f060db07d95283cf35a723391
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-10 17:43:09 +0100

    commit

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -38,7 +38,7 @@ class _ChatMessageState extends State<ChatMessage> {
             ? MainAxisAlignment.end
             : MainAxisAlignment.start,
         children: [
-          
+          if (widget.sender == MessageSender.user) 
           ChatBubble(
             clipper: ChatBubbleClipper8(
                 type: widget.sender == MessageSender.user
