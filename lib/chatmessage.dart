00e009c67cada505a44088fcc1e67baa41342642
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-10 17:46:12 +0100

    commit

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -44,7 +44,7 @@ class _ChatMessageState extends State<ChatMessage> {
               padding: EdgeInsets.only(top: 20),
               child: CircleAvatar(
                 radius: 15,
-                backgroundColor: Vx.zinc100,
+                backgroundColor: Vx.zinc200,
               ),
             ),
           ChatBubble(
