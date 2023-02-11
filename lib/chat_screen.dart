3831a47c06c8ca6b32626fe005c47ced8d62ae80
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-11 20:46:50 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chat_gpt_02/reply_preview.dart';
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_bubble/chat_bubble.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -159,7 +160,81 @@ class _ChatScreenState extends State<ChatScreen> {
         : const SizedBox();
   }
 
-  Widget
+  Widget chatMessage() {
+    return SwipeTo(
+      onRightSwipe: () {
+        setState(() {
+          isResponse = true;
+          replyMessage = ChatMessage(
+            text: widget.text,
+            sender: widget.sender,
+          );
+          debugPrint("Replying to: ${replyMessage!.text}");
+        });
+      },
+      child: Row(
+        mainAxisAlignment: widget.sender == MessageSender.user
+            ? MainAxisAlignment.end
+            : MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (widget.sender == MessageSender.bot)
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                child: const CircleAvatar(
+                  radius: 15,
+                  backgroundColor: Vx.zinc200,
+                  backgroundImage: AssetImage("assets/images/chatgpt_icon.png"),
+                ),
+              ),
+            ),
+          ChatBubble(
+            clipper: ChatBubbleClipper8(
+                type: widget.sender == MessageSender.user
+                    ? BubbleType.sendBubble
+                    : BubbleType.receiverBubble),
+            margin: const EdgeInsets.only(top: 20),
+            backGroundColor:
+                widget.sender == MessageSender.user ? Vx.green500 : Vx.zinc200,
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.7,
+              ),
+              child: Text(
+                widget.text.trim(),
+                style: TextStyle(
+                  color: widget.sender == MessageSender.user
+                      ? Colors.white
+                      : Colors.black,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+          if (widget.sender == MessageSender.user)
+            Padding(
+              padding: const EdgeInsets.only(top: 23),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                child: const CircleAvatar(
+                  radius: 15,
+                  backgroundColor: Vx.green500,
+                  child: Icon(
+                    size: 18,
+                    color: Colors.white,
+                    Icons.person,
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
