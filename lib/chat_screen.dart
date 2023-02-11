78af3c072f3f9f310be3b35bb0482f30bfbe3c24
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-11 19:17:32 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -124,6 +124,7 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
           child: IconButton(
             icon: const Icon(
+              color: Vx.zinc200,
               Icons.send,
             ),
             onPressed: () {
