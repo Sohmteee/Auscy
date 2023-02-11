d62e1d6d001d55ebc536be558791fed95725b7ae
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-11 19:05:44 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -147,82 +147,11 @@ class _ChatScreenState extends State<ChatScreen> {
         : const SizedBox();
   }
 
-  Widget replyPreview(MessageSender sender, String text) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      width: double.maxFinite,
-      decoration: const BoxDecoration(
-          color: Vx.zinc200,
-          borderRadius: BorderRadius.vertical(
-            top: Radius.circular(20),
-          )),
-      child: IntrinsicHeight(
-        child: Row(
-          children: [
-            Container(
-              width: 5,
-              decoration: const BoxDecoration(
-                color: Vx.green500,
-                borderRadius: BorderRadius.horizontal(
-                  left: Radius.circular(8),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Vx.zinc300,
-                  borderRadius: BorderRadius.horizontal(
-                    right: Radius.circular(8),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            sender == MessageSender.user ? "Me" : "ChatGPT",
-                            style: const TextStyle(
-                              color: Vx.green500,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                isResponse = false;
-                              });
-                            },
-                            child: const Icon(
-                              size: 20,
-                              Icons.close,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        text.trim(),
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
         appBar: AppBar(
           leading: const Padding(
