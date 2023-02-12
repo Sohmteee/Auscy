4beeb493f998d86fc564a77f26e9bef1d22f42c5
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-12 21:57:53 +0100

    commit

--- lib/provider.dart
+++ lib/provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class MyProvider extends ChangeNotifier {
   bool _isResponse = false;
-  bool getIsResponse() => _isResponse;
+  bool get isResponse => _isResponse;
 
   void toggleResponse() {
     _isResponse = !_isResponse;
