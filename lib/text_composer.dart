d3a8e405df6865b371901e5cee40cf7985489470
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-12 21:22:59 +0100

    commit

---
+++ lib/text_composer.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class TextComposer extends StatefulWidget {
+  const TextComposer({super.key});
+
+  @override
+  State<TextComposer> createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              buildReplyHoverBubble(),
+              Container(
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Vx.zinc200,
+                  borderRadius: isResponse
+                      ? const BorderRadius.vertical(bottom: Radius.circular(20))
+                      : BorderRadius.circular(20),
+                ),
+                child: TextField(
+                  autofocus: true,
+                  controller: controller,
+                  textCapitalization: TextCapitalization.sentences,
+                  onSubmitted: (value) => _sendMessage(),
+                  decoration: InputDecoration.collapsed(
+                    hintText: hintText,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(width: 10),
+        Container(
+          decoration: const BoxDecoration(
+            color: Vx.green500,
+            shape: BoxShape.circle,
+          ),
+          child: IconButton(
+            icon: const Icon(
+              color: Vx.white,
+              Icons.send,
+            ),
+            onPressed: () {
+              _sendMessage();
+            },
+          ),
+        ),
+      ],
+    ).px16();
+  }
+}
