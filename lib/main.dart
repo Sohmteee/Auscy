aa6e0e716ca1f7e05e03923f0a63de4269abe1b1
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-12 22:02:02 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
-        fo
+        fontFamily: "Poppins",
         useMaterial3: true,
       ),
       home: const ChatScreen(),
