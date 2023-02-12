3495994acd0e3e6e6d53429cc7fba1d7018c707d
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-12 21:02:22 +0100

    commit

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -31,7 +31,7 @@ class _ChatMessageState extends State<ChatMessage> {
           replyMessage = ChatMessage(
             text: widget.text,
             sender: widget.sender,
-            context: _ChatScreenState().context,
+            context: context,
           );
           debugPrint("Replying to: ${replyMessage!.text}");
         });
