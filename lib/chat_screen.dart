0a5b01be8a600b166d5ffc668cbd8e1d743ca189
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-13 22:37:16 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -290,10 +290,106 @@ class _ChatScreenState extends State<ChatScreen> {
                                 children: [
                                   // reply preview
                                   model.isResponse
-                                      ? ReplyPreview(
+                                      ? /* ReplyPreview(
                                           sender: replyMessage!.sender,
                                           text: replyMessage!.text,
                                           setResponse: setResponse,
+                                        ) */
+                                        Container(
+                                          padding: const EdgeInsets.all(10),
+                                          width: double.maxFinite,
+                                          decoration: const BoxDecoration(
+                                              color: Vx.zinc200,
+                                              borderRadius:
+                                                  BorderRadius.vertical(
+                                                top: Radius.circular(20),
+                                              )),
+                                          child: IntrinsicHeight(
+                                            child: Row(
+                                              children: [
+                                                Container(
+                                                  width: 5,
+                                                  decoration:
+                                                      const BoxDecoration(
+                                                    color: Vx.green500,
+                                                    borderRadius:
+                                                        BorderRadius.horizontal(
+                                                      left: Radius.circular(8),
+                                                    ),
+                                                  ),
+                                                ),
+                                                Expanded(
+                                                  child: Container(
+                                                    decoration:
+                                                        const BoxDecoration(
+                                                      color: Vx.zinc300,
+                                                      borderRadius: BorderRadius
+                                                          .horizontal(
+                                                        right:
+                                                            Radius.circular(8),
+                                                      ),
+                                                    ),
+                                                    child: Padding(
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              5),
+                                                      child: Column(
+                                                        crossAxisAlignment:
+                                                            CrossAxisAlignment
+                                                                .start,
+                                                        children: [
+                                                          Row(
+                                                            mainAxisAlignment:
+                                                                MainAxisAlignment
+                                                                    .spaceBetween,
+                                                            children: [
+                                                              Text(
+                                                                widget.sender ==
+                                                                        MessageSender
+                                                                            .user
+                                                                    ? "You"
+                                                                    : "ChatGPT",
+                                                                style:
+                                                                    const TextStyle(
+                                                                  color: Vx
+                                                                      .green500,
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .bold,
+                                                                ),
+                                                              ),
+                                                              GestureDetector(
+                                                                onTap: () {
+                                                                  setState(() {
+                                                                    widget.setResponse(
+                                                                        false);
+                                                                  });
+                                                                },
+                                                                child:
+                                                                    const Icon(
+                                                                  size: 20,
+                                                                  Icons.close,
+                                                                ),
+                                                              ),
+                                                            ],
+                                                          ),
+                                                          const SizedBox(
+                                                              height: 5),
+                                                          Text(
+                                                            widget.text.trim(),
+                                                            maxLines: 3,
+                                                            overflow:
+                                                                TextOverflow
+                                                                    .ellipsis,
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
                                         )
                                       : const SizedBox(),
                                   Container(
