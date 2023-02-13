ef5d5d24cfb020b616a380d010146e0aed3dd90c
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-13 23:17:57 +0100

    commit

--- lib/provider.dart
+++ lib/provider.dart
@@ -4,7 +4,7 @@ class MyProvider extends ChangeNotifier {
   bool _isResponse = false;
   bool get isResponse => _isResponse;
 
-  set isResponse(bool value) {
+  set setResponse(bool value) {
     _isResponse = value;
     notifyListeners();
   }
