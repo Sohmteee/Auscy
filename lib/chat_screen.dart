f02a44fd3de73ad83e5f775cd268e11afef11e15
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-13 22:18:49 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -293,7 +293,7 @@ class _ChatScreenState extends State<ChatScreen> {
                                     padding: const EdgeInsets.all(15),
                                     decoration: BoxDecoration(
                                       color: Vx.zinc200,
-                                      borderRadius: model.isResponse
+                                      borderRadius: isResponse
                                           ? const BorderRadius.vertical(
                                               bottom: Radius.circular(20))
                                           : BorderRadius.circular(20),
