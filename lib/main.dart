978e7ca38d0d8f6aeabb6263c4d04f7e4e944f19
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-15 10:40:02 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    ToastContext().init(context);
     return MaterialApp(
       title: 'ChatGPT',
       debugShowCheckedModeBanner: false,
