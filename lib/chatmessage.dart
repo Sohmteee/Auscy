72690347a30b045f69ecaa74074f78ea6e52f2ab
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-15 10:55:28 +0100

    commit

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -105,7 +105,7 @@ class _ChatMessageState extends State<ChatMessage> with ChangeNotifier {
                     ),
                   ),
                 ),
-                if (widget.sender == MessageSender.user && ((messages.length > 2) ))
+                if (widget.sender == MessageSender.user && ((messages.length > 1) ? (messages[messages.length - 2].sender != MessageSender.user) ))
                   Padding(
                     padding: const EdgeInsets.only(top: 23),
                     child: Card(
