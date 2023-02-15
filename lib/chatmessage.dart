26a8a6847e03c9f11bb12a67b65419d1925b1d6b
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-15 10:47:55 +0100

    commit

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -81,7 +81,7 @@ class _ChatMessageState extends State<ChatMessage> with ChangeNotifier {
                         duration: Toast.lengthShort, gravity: Toast.bottom);
                   },
                   child: ChatBubble(
-                    clipper: ChatBubbleClipper7(
+                    clipper: ChatBubbleClipper10(
                         type: widget.sender == MessageSender.user
                             ? BubbleType.sendBubble
                             : BubbleType.receiverBubble),
