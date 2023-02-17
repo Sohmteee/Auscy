97011150739d47fd3f0466437bf2447c81b4fbee
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-17 13:48:55 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -84,8 +84,7 @@ class _ChatScreenState extends State<ChatScreen> {
       Vx.log(response!.choices[0].text);
       insertNewData(response.choices[0].text.trim());
     } catch (e) {
-      insertNewData(
-          e);
+      insertNewData(e.toString());
     }
   }
 
