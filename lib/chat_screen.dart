e544bf176a8e51844dc188121fb56660f216ef1f
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-17 12:54:00 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -89,7 +89,7 @@ class _ChatScreenState extends State<ChatScreen> {
 
   void insertNewData(String response) {
     ChatMessage botMessage = ChatMessage(
-      text: response.trim() != "" ? response : "",
+      text: response.trim() != "" ? response : "An error occured. Couldn't get",
       sender: MessageSender.bot,
     );
 
