748ddb0e0572da3d920f3b30523d6182fa1a087e
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-17 14:13:42 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -61,7 +61,7 @@ class _ChatScreenState extends State<ChatScreen> {
     controller.clear();
 
     String prompt = "";
-    int start = (messages.length <= 10) ? 0 : (messages.length - 10);
+    int start = (messages.length < 10) ? 0 : (messages.length - 10);
 
     List<String> last10Texts =
         messages.sublist(start).map((message) => message.text).toList();
@@ -269,7 +269,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: [
                   Flexible(
                       child: ListView.builder(
-                    reverse: false,
+                    reverse: true,
                     padding: Vx.m8,
                     itemCount: messages.length,
                     itemBuilder: (context, index) {
