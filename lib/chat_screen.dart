d624f094be1cceea45d08f8ab13dbbd1f65dfcfc
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-18 14:48:56 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -77,7 +77,7 @@ class _ChatScreenState extends State<ChatScreen> {
       Vx.log(response!.choices[0].text);
       insertNewData(response.choices[0].text.trim());
     } catch (e) {
-      insertNewData("Sorry, an error occured while ");
+      insertNewData("Sorry, an error occured while trying to respond");
     }
     scrollToBottom();
   }
