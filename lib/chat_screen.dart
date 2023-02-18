8344e3bf3e5ef4030ed740eb1fdc14f27bb46787
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-18 15:11:35 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -160,7 +160,6 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 60,
         leading: const Padding(
           padding: EdgeInsets.all(8.0),
           child: CircleAvatar(
