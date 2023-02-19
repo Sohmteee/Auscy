f17766392861c4aa641d3c1d26b1f5c5fea600f0
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-19 17:50:07 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -166,12 +166,16 @@ class _ChatScreenState extends State<ChatScreen> {
             backgroundImage: AssetImage("assets/images/chatgpt_icon.png"),
           ),
         ),
-        title: const Text(
-          "ChatGPT",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 25,
-          ),
+        title: Row(
+          children: [
+            const Text(
+              "ChatGPT",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+          ],
         ),
         actions: [
           Container(
