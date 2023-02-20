b90833e68cd12d8cebb2e7a00760bf47b0ce1f75
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-20 18:11:58 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'ChatGPT',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.black,
+        primarySwatch: Colors.grey,
         fontFamily: "Poppins",
         useMaterial3: true,
       ),
