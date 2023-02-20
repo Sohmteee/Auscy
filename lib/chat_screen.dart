dd0064787ed7f728f3d1ce34b287569bc47e0dd4
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-20 18:14:20 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -108,6 +108,7 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   Widget _buildTextComposer() {
+    
     return Row(
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
