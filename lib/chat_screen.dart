9710423eba12bb846a81f28c363d91d9e12e613a
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-20 18:22:41 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -144,7 +144,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   onChanged: (value) {
                     setState(() {
-                      toggleSend();
+                      
                     });
                   },
                 ),
