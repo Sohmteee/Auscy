ac6a83b94884cc4024846bbf6e8050e5adada391
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-21 23:43:24 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:toast/toast.dart';
 import 'chat_screen.dart';
@@ -6,7 +7,8 @@ import 'chat_screen.dart';
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
-  
+  SystemChrome.setEnabledSystemUIMode([SystemUiOverlay.bottom])
+
   runApp(const MyApp());
 }
 
