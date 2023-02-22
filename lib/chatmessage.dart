fdff5e307807e8cfc0a101405145605b3cc34f30
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-22 01:23:54 +0100

    commit

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -80,7 +80,25 @@ class _ChatMessageState extends State<ChatMessage> with ChangeNotifier {
                 maxWidth: MediaQuery.of(context).size.width * 0.7,
               ),
               child: widget.sender == MessageSender.bot
-                  ? TypewriterAnimatedText(
+                  ? 
+                  AnimatedTextKit(
+  animatedTexts: [
+    TypewriterAnimatedText(
+      'Hello world!',
+      textStyle: const TextStyle(
+        fontSize: 32.0,
+        fontWeight: FontWeight.bold,
+      ),
+      speed: const Duration(milliseconds: 2000),
+    ),
+  ],
+  
+  totalRepeatCount: 4,
+  pause: const Duration(milliseconds: 1000),
+  displayFullTextOnTap: true,
+  stopPauseOnTap: true,
+)
+                  TypewriterAnimatedText(
                     widget.text.trim(),
                       
                       
