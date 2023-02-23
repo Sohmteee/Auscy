c6b9f0910c56d88ddd6136b2d02be53ee13e33bb
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-23 02:00:22 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -163,6 +163,7 @@ class _ChatScreenState extends State<ChatScreen> {
               icon,
             ),
             onPressed: () {
+              
               _sendMessage();
             },
           ),
