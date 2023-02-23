02ca9b27e7ffb4877802d71630f406935dea7909
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-23 10:20:13 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -55,7 +55,7 @@ class _ChatScreenState extends State<ChatScreen> {
       body: jsonEncode(
         {
           'model': 'text-davinci-003',
-          'prompt' : prompt
+          'prompt' : prompt.trim(),
         }
       )
     );
