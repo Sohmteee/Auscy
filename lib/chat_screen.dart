d02735e56616ad9e8ab6d9a0106994eb8addc733
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-11 13:15:05 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -89,6 +89,7 @@ class _ChatScreenState extends State<ChatScreen> {
     if (text.trim().isEmpty) return;
     
     String? punctuation = endsWithPunctuation(text.trim());
+    
     ChatMessage message = ChatMessage(
       text: text.trim(),
       sender: MessageSender.user,
