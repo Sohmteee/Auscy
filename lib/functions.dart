3429370416d6392960378e74686c73a9bf04a74b
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-12 16:00:15 +0100

    commit

--- lib/functions.dart
+++ lib/functions.dart
@@ -3,6 +3,7 @@ import 'package:chat_gpt_02/chatmessage.dart';
 JSONtoChatMessage(Map<String, dynamic> json) {
   return ChatMessage(
     text: json['text'],
-    sender: ['sender'],
+    sender: json['sender'],
+    
   );
 }
