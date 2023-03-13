1137e0509dfff5fcad141dd339bbb720ea602f3a
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-14 00:36:51 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -247,7 +247,7 @@ class _ChatScreenState extends State<ChatScreen> {
           children: [
             Flexible(
               child: StreamBuilder(
-                future: 
+                
                 builder: (context, snapshot) {
                   return 
                 },
