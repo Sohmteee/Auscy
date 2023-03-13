82dfb5931f9cca49c6c3992653e1e17c29c93c14
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-14 00:53:26 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -261,7 +261,7 @@ class _ChatScreenState extends State<ChatScreen> {
                     reverse: true,
                     itemCount: messages.length,
                     itemBuilder: (context, index) {
-                      int id = user
+                      int id = user[index]
                       return user.reversed.toList()[index];
                     },
                   );
