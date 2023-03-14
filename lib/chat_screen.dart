53880b0658b253e48b4844b74db49227d616ebe1
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-14 01:21:48 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -262,7 +262,7 @@ class _ChatScreenState extends State<ChatScreen> {
                     itemCount: messages.length,
                     itemBuilder: (context, index) {
                       final user = users[index];
-                      return Text(user["id"]);
+                      return ListTile(title: ,)
                     },
                   );
                 },
