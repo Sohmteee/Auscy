091a4d868c86b7fc18e4a59138b2381eaccb420a
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-14 14:03:16 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -77,7 +77,7 @@ class _ChatScreenState extends State<ChatScreen> {
       "what",
       "when",
       "where",
-      "why", "how", "who", "which", "whose", "can", "do", "d"
+      "why", "how", "who", "which", "whose", "can", "do", "does", 
     ];
   }
 
