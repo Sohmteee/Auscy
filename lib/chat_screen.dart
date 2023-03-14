4bbde017bdcd4dcf8f04946c9b2ecb15963a5570
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-14 14:10:29 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -72,7 +72,7 @@ class _ChatScreenState extends State<ChatScreen> {
     return last20Texts.join('\n');
   }
 
-  bool isQuestion(String string) => words
+  bool isQuestion(String string) => words.any((element) => false)
 
   void _sendMessage() async {
     String text = controller.text;
