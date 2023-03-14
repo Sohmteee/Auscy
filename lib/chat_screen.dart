8a39cd28a6f60a52943045af392e0ad2d29ff068
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-14 14:22:25 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -75,7 +75,7 @@ class _ChatScreenState extends State<ChatScreen> {
   bool isQuestion(String string) {
     List<String> statements = string.split(RegExp(r"[\s.?!]"));
     for(String statement in statements) {
-      if (questionWords.any((word) => statement.toLowerCase().startsWith(word)))
+      if (questionWords.any((word) => statement.toLowerCase().startsWith(word)) && !statement)
     }
     
     return questionWords.any((word) => string.startsWith(word) && string.contains("?"));
