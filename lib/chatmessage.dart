94115845620d75b068c83dc1da704aad9e84a9df
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-15 21:29:41 +0100

    commit

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -101,23 +101,14 @@ class _ChatMessageState extends State<ChatMessage> {
                   const SizedBox(height: 2),
                   Container(
                     color: Colors.white,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Align(
-                          alignment: Alignment.bottomRight,
-                          child: Text(
-                            DateFormat("h:mm a").format(widget.time),
-                            style: TextStyle(
-                              color: widget.sender == MessageSender.user
-                                  ? Colors.grey.shade100
-                                  : Colors.grey.shade200,
-                              fontSize: 10,
-                            ),
-                          ),
-                        ),
-                      ],
+                    child: Text(
+                      DateFormat("h:mm a").format(widget.time),
+                      style: TextStyle(
+                        color: widget.sender == MessageSender.user
+                            ? Colors.grey.shade100
+                            : Colors.grey.shade200,
+                        fontSize: 10,
+                      ),
                     ),
                   )
                 ],
