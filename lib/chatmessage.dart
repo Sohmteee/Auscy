663beb9a805b30ec84dc2086e77a07933f468645
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-15 21:11:13 +0100

    commit

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -95,7 +95,7 @@ class _ChatMessageState extends State<ChatMessage> {
                     ),
                   ),
                   const SizedBox(height: 2),
-                  Text(DateFormat("h:mma").format(date);)
+                  Text(DateFormat("h:mma").format(date))
                 ],
               ),
             ),
