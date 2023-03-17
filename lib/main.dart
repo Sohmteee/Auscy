8afeff0c5ae17582cd1b8571ca002f7d13f71238
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-17 03:58:49 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'chat_screen.dart';
 
 Future main() async {
