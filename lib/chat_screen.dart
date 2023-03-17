35e416b61c30aa75a7dc6d106fbdec56d4e76d40
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-17 05:10:53 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -199,7 +199,7 @@ class _ChatScreenState extends State<ChatScreen> {
         const SizedBox(width: 10),
         Container(
           decoration: const BoxDecoration(
-            color: Vx.gray600,
+            color: Vx.black,
             shape: BoxShape.circle,
           ),
           child: IconButton(
