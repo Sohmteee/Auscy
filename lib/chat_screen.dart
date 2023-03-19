c4f5ed6567c1a4a97751b90e1569fe51c3b62b8e
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-19 23:00:31 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -142,7 +142,7 @@ class _ChatScreenState extends State<ChatScreen> {
   Dialog errorDialog(e) {
 
     return Dialog(
-      shape: Rec,
+      shape: Rectangular,
       child: Container(
         padding: const EdgeInsets.all(20),
         decoration: BoxDecoration(
