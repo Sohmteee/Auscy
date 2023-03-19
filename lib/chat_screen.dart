08934eda9a70358aedf28f86e8b648d14496db3b
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-19 23:07:02 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -142,7 +142,7 @@ class _ChatScreenState extends State<ChatScreen> {
   Dialog errorDialog(e) {
     return Dialog(
       // alignment: Alignment.bottomCenter,
-      backgroundColor: Vx.red100,
+      backgroundColor: Vx.red00,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),
       ),
