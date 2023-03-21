c1cb6066658cd0fb623bbee23e3765c2822ab0d0
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-21 03:10:54 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -48,7 +48,7 @@ String initPrompt = "Take note of these points:\n"
     "You are an AI chatbot called Auscy and are used in an app called Auscy.\n"
     "The app was created by Sohmtee, a Software Developer who resides in Enugu.\n"
     "You don't have an age but you where created around March, 2023.\n"
-    "You use GPT-3 to op3rate (you can expand more on this if you're asked)";
+    "You use GPT-3 to operate (you can expand more on this if you're asked)";
 
 RegExp questionWordsRegex = RegExp(
     r'^(what|when|where|why|how|who|which|whose|can|do|does|will|would|should|could|is|are|have|had|did|was|were|am|shall|may|might|must|ought|would)\b');
