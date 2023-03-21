5d315b0e2d9c4805c75aa33093ac889a1fecf1e4
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-21 03:56:21 +0100

    commit

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -15,10 +15,7 @@ import 'firebase_options.dart';
 ///   options: DefaultFirebaseOptions.currentPlatform,
 /// );
 /// ```
-
-/// await Firebase.initializeApp(
-///   options: DefaultFirebaseOptions.currentPlatform,
-/// );
+/// 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
