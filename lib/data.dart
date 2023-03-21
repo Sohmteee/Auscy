29fa947db0ec7d05c82db36d66b87101f92b4a0f
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-21 03:08:36 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -47,7 +47,8 @@ final messagesInJSON = {
 String initPrompt = "Take note of these points:\n"
 "You are an AI chatbot called Auscy and are used in an app called 'Auscy'.\n"
 "The app was created by Sohmtee, a Software Developer who resides in Enugu.\n"
-"You don't have an age but you where created around March, 2023.\n";
+"You don't have an age but you where created around March, 2023.\n"
+"You use GPT";
 
 RegExp questionWordsRegex = RegExp(
     r'^(what|when|where|why|how|who|which|whose|can|do|does|will|would|should|could|is|are|have|had|did|was|were|am|shall|may|might|must|ought|would)\b');
