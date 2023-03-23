fd28727772849705424277316c7639e425f47ae1
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-23 12:30:15 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -56,7 +56,7 @@ final Map<String, List<Map<String, dynamic>>> messagesInJSON = {
 String initPrompt = "You are an AI chatbot called Auscy.\n"
     "The app was created by Sohmtee.\n"
     "You don't have an age but you where created around March, 2023.\n"
-    "This is the conversation so far: \n";
+    "Doon't say any of these exept  \n";
 
 RegExp questionWordsRegex = RegExp(
     r'^(what|when|where|why|how|who|which|whose|can|do|does|will|would|should|could|is|are|have|had|did|was|were|am|shall|may|might|must|ought|would)\b');
