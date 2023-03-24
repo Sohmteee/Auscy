15cd41e9d5734ea651a6dd2cba6a39d56bdbcc31
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-24 12:53:05 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -1,7 +1,6 @@
 import 'package:auscy/chatmessage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 enum MessageSender {
   user,
