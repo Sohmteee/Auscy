025b1778a91e4840a4c93f40e941961d2c43a6b8
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-24 12:36:44 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -120,7 +120,7 @@ class _ChatScreenState extends State<ChatScreen> {
 
     setState(() {
       messages.add(message);
-      (box.get("messages") == null) ? : ;
+      (box.get("messages") == null) ? box.put("messages", value): ;
       // messagesInJSON['messages']?.add(message.toJSON());
       _isTyping = true;
       icon = Icons.mic;
