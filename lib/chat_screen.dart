831a7ab2dbad0d400485675f7f326b57179df706
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-24 12:31:45 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:hive/hive.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:http/http.dart' as http;
 
