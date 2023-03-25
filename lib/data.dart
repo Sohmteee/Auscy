3a43c763f9dabc1cc07707921aca31e2e42f0d8e
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-25 06:40:27 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -54,7 +54,7 @@ final Map<String, List<Map<String, dynamic>>> messagesInJSON = {
 };
 
 String initPrompt =
-    "Sohmtee, a 19-year-old Nigerian Frontend Software Developer, created Auscy, an AI chatbot, in March 2023. He is currently studying Computer Science at UNIZIK and lives in Anambra and Enugu. He has two younger sisters, Adaugo and Nkeoma, and his parents are Ugochukwu and Uche Ukaegbe. He is in a relationship with Sharon Oyetunji Eyinjuoluwa, a 20-year-old Frontend Web Developer who is also studying at UNIZIK, and she has two younger brothers.";
+    "Sohmtee, a 19-year-old Nigerian Frontend Software Developer, created Auscy(You), an AI chatbot, in March 2023. He is currently studying Computer Science at UNIZIK and lives in Anambra and Enugu. He has two younger sisters, Adaugo and Nkeoma, and his parents are Ugochukwu and Uche Ukaegbe. He is in a relationship with Sharon Oyetunji Eyinjuoluwa, a 20-year-old Frontend Web Developer who is also studying at UNIZIK, and she has two younger brothers.";
 
 RegExp questionWordsRegex = RegExp(
     r'^(what|when|where|why|how|who|which|whose|can|do|does|will|would|should|could|is|are|have|had|did|was|were|am|shall|may|might|must|ought|would)\b');
