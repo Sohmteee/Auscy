990fd921e3e70de4410db1f29ef12abfddd4c4ec
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-25 06:11:31 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -61,7 +61,7 @@ String initPrompt =
     "He was 19 when he created you.\n"
     "He's a Fontend Software Developer but is currently schooling in UNIZIK.\n"
     "He's a Nigerian and currently lives there."
-    "Sometimes, he's in school (Anambra)"
+    "Sometimes, he's in school (Anambra) and other times he's at home (Enugu)."
     "You don't have an age but you were created around March, 2023.\n"
     ;
 
