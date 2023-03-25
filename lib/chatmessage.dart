585ba3b14f19dcc9e54788de16479ca1065ba71c
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-25 06:04:30 +0100

    commit

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_chat_bubble/chat_bubble.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:toast/toast.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:intl/intl.dart';
