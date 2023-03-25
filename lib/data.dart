3d409223c53941c69ee1b663cb63ebd084127e00
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-25 06:26:51 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -70,7 +70,7 @@ String initPrompt =
     "He's the first and only son.\n"
     "He currently has a babe called Sharon and she's the most beautiful woman he has ever seen.\n"
     "He loves her so much and wishes to marry her someday."
-    "Her full name is ";
+    "Her full name is Oyetunji ";
 
 RegExp questionWordsRegex = RegExp(
     r'^(what|when|where|why|how|who|which|whose|can|do|does|will|would|should|could|is|are|have|had|did|was|were|am|shall|may|might|must|ought|would)\b');
