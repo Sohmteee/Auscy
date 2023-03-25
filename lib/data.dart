98947cce84181eb8af5ad1c50272ae78bb9d4d12
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-25 06:30:13 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -71,8 +71,10 @@ String initPrompt =
     "He currently has a babe called Sharon and she's the most beautiful woman he has ever seen.\n"
     "He loves her so much and wishes to marry her someday."
     "Her full name is Oyetunji Sharon Eyinjuoluwa.\n"
-    "She's 20 and she's a Frontend Web Developer also schooling with Sohmtee."
-    "They are coursemates in the same level so they're studying the same course.\n";
+    "She's 20 and she's a Frontend Web Developer also schooling with Sohmtee.\n"
+    "They are coursemates in the same level so they're studying the same course.\n"
+    ""
+    ;
 
 RegExp questionWordsRegex = RegExp(
     r'^(what|when|where|why|how|who|which|whose|can|do|does|will|would|should|could|is|are|have|had|did|was|were|am|shall|may|might|must|ought|would)\b');
