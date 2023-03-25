e966a8caadf007e83f75e8de014dbbac2f97d04b
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-25 05:50:18 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -186,6 +186,7 @@ class _ChatScreenState extends State<ChatScreen> {
     setState(() {
       _isTyping = false;
       messages.add(botMessage);
+      box.put("messages", messages);
       messagesInJSON['messages']?.add(botMessage.toJSON());
       // createMessage(username: "Sohmtee");
     });
