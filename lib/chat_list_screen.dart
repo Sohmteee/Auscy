57f89933f5268b45e5cd24ee4510141fd392e2da
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-26 23:32:28 +0100

    commit

--- lib/chat_list_screen.dart
+++ lib/chat_list_screen.dart
@@ -10,6 +10,55 @@ class ChatListScreen extends StatefulWidget {
 class _ChatListScreenState extends State<ChatListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundImage: AssetImage("assets/images/auscy_icon.png"),
+          ),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              "Auscy",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+            GestureDetector(
+              onTap: () => setState(() {
+                messages.clear();
+                box.put("messages", messages);
+                _isTyping = false;
+                setState(() {
+                  icon = controller.text.trim().isNotEmpty
+                      ? Icons.send
+                      : Icons.mic;
+                });
+              }),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Vx.zinc900,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Text(
+                  "Reset",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
