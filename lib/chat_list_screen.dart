de45e0e7b7f75239f42fd489e7e747ba43b34321
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-26 23:37:05 +0100

    commit

---
+++ lib/chat_list_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ChatListScreen extends StatefulWidget {
+  const ChatListScreen({super.key});
+
+  @override
+  State<ChatListScreen> createState() => _ChatListScreenState();
+}
+
+class _ChatListScreenState extends State<ChatListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundImage: AssetImage("assets/images/auscy_icon.png"),
+          ),
+        ),
+        title: const Text(
+          "Auscy",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+          ),
+        ),
+      ),
+      body: SafeArea(child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Container(
+            padding: const EdgeInsets.all(10),
+            
+          );
+        },
+      )),
+    );
+  }
+}
