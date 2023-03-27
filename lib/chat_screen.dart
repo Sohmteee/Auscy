090381cca76fb59a43bfd1709280cac077f2ab50
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-28 00:39:34 +0100

    commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -11,13 +11,12 @@ import 'chatmessage.dart';
 import 'data.dart';
 
 class ChatScreen extends StatefulWidget {
-  const ChatScreen({
+  ChatScreen({
     super.key,
     required this.messages,
   });
-  
-  List messages = [];
 
+  List messages = [];
 
   @override
   State<ChatScreen> createState() => _ChatScreenState();
