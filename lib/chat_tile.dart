fd90b56dcd886421f21845418964f5e415ee37b3
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-30 10:39:56 +0100

    commit

--- lib/chat_tile.dart
+++ lib/chat_tile.dart
@@ -33,6 +33,7 @@ class _ChatTileState extends State<ChatTile> {
       },
       child: Container(
         padding: const EdgeInsets.all(10),
+        width: double.maxFinite,
         child: Row(
           children: [
             const CircleAvatar(
