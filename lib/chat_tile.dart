673560d99821ad211b0166129783fe6b72e66837
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-30 10:36:39 +0100

    commit

--- lib/chat_tile.dart
+++ lib/chat_tile.dart
@@ -25,7 +25,7 @@ class _ChatTileState extends State<ChatTile> {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) =>  ChatScreen(title: ,),
+            builder: (context) =>  ChatScreen(title: widget.text!,),
           ),
         );
       },
