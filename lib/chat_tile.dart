7144dfb9a76dd450d50a220bfb980a0f88963b1b
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-30 10:51:35 +0100

    commit

--- lib/chat_tile.dart
+++ lib/chat_tile.dart
@@ -32,7 +32,48 @@ class _ChatTileState extends State<ChatTile> {
         );
       },
       child: ListTile(
-        
-      )
+        leading: const CircleAvatar(
+          backgroundImage: AssetImage("assets/images/auscy_icon.png"),
+        ),
+        title: Text(
+          widget.text ?? "No Title",
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
   }
 }
+
+/* Container(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage("assets/images/auscy_icon.png"),
+            ),
+            const SizedBox(width: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.text ?? "No Title",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  widget.message,
+                  style: const TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+       */
