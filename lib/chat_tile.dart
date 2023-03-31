6b7ad7509a5aa5ffdd45d63bd16cfddd2c976a06
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-31 11:43:36 +0100

    commit

--- lib/chat_tile.dart
+++ lib/chat_tile.dart
@@ -51,6 +51,7 @@ class _ChatTileState extends State<ChatTile> {
         subtitle: Text(
           widget.message,
           overflow: TextOverflow.ellipsis,
+          maxLines: 1,
           style: const TextStyle(
             fontSize: 12,
           ),
