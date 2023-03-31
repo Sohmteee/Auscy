18c6670aa70f009fad13d76332d1c78713dfd75f
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-31 11:40:56 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -30,7 +30,7 @@ List chatList = [
   ChatTile(
     text: "Elon Musk",
     index: 1,
-    message: "How are you?",
+    message: "Some very interesting facts about Elon Musk include",
     time: DateTime.now(),
   ),
 ];
