58eba73f36f02e999c14785a9be790f61b1cbc6e
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-31 12:11:21 +0100

    commit

--- lib/functions.dart
+++ lib/functions.dart
@@ -12,8 +12,11 @@ jsonToChatMessage(Map<String, dynamic> json) {
   );
 }
 
+json
+
 jsonToChatTile(Map<String, dynamic> json) {
   return ChatTile(
+    title: json['title'],
     messages: json['messages'],
     time: json['time'],
   );
