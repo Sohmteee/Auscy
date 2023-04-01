d2cf5edcac58b3afae0417e5fa2b5c1fb68097a0
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-04-01 23:00:00 +0100

    commit

--- lib/chat_tile.dart
+++ lib/chat_tile.dart
@@ -98,14 +98,18 @@ class _ChatTileState extends State<ChatTile> {
             fontSize: 12,
           ),
         ),
-        trailing: Text(
-          DateFormat("h:mm a").format(widget.time),
-          style: const TextStyle(
-            color: Vx.gray400,
-            fontSize: 10,
-          ),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              DateFormat("h:mm a").format(widget.time),
+              style: const TextStyle(
+                color: Vx.gray400,
+                fontSize: 10,
+              ),
+            ),
+          ],
         ),
-        
       ),
     );
   }
