5b92c3dd1fae7a7fdeddd47036a6dc0699a943a8
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-08-26 14:51:35 +0100

    commit

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_chat_bubble/chat_bubble.dart';
 import 'package:toast/toast.dart';
-import 'package:velocity_x/velocity_x.dart';
+// import 'package:velocity_x/velocity_x.dart';
 import 'package:intl/intl.dart';
 
 import '../data.dart';
